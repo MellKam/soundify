298bf5b695ffd814dc528e403cf2b9ff6919aca8
Artem Melnyk <melkam464@gmail.com>
2023-02-11 14:23:03 +0100

    upd

--- scripts/bundle_web.ts
+++ scripts/bundle_web.ts
@@ -1,14 +1,9 @@
-import { emit } from "https://deno.land/x/emit@0.15.0/mod.ts";
+import { bundle } from "https://deno.land/x/emit@0.15.0/mod.ts";
 import { createCache } from "https://deno.land/x/deno_cache@0.4.1/mod.ts";
-import {
-	fromFileUrl,
-	join,
-	relative,
-} from "https://deno.land/std@0.177.0/path/mod.ts";
 
 const cache = createCache();
 
-const files = await emit("./mod.ts", {
+const { code: bundledCode } = await bundle("./mod.ts", {
 	load: (specifier) => {
 		if (specifier.endsWith(".deno.ts")) {
 			const baseLength = specifier.length - ".deno.ts".length;
@@ -16,21 +11,17 @@ const files = await emit("./mod.ts", {
 		}
 		return cache.load(specifier);
 	},
+	compilerOptions: {
+		sourceMap: false,
+		inlineSources: false,
+		inlineSourceMap: false,
+	},
 });
 
-const cwd = Deno.cwd();
-await Promise.all(
-	Object.keys(files).map(async (fileURL) => {
-		const relativePath = relative(cwd, fromFileUrl(fileURL)).replace(
-			".ts",
-			".js",
-		);
-
-		await Deno.writeTextFile(
-			join(cwd, "dist", "web", relativePath),
-			files[fileURL].replace(/\/\/# sourceMappingURL=.*/, ""),
-		);
-	}),
+await Deno.writeTextFile(
+	"./dist/web.mjs",
+	bundledCode.replace(/\/\/# sourceMappingURL=.*\n/, ""),
 );
+await Deno.writeTextFile("./dist/web.d.ts", 'export * from "./mod.js";\n');
 
-// console.log("Done.");
+console.log("Done.");
---
+++ tests/testEnv.ts
@@ -0,0 +1,43 @@
+import {
+	cleanEnv,
+	makeValidator,
+} from "https://deno.land/x/envalid@0.1.2/mod.ts";
+
+import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+
+export interface TestEnv extends Record<string, string> {
+	SPOTIFY_CLIENT_ID: string;
+	SPOTIFY_CLIENT_SECRET: string;
+	SPOTIFY_REFRESH_TOKEN: string;
+}
+
+export const getTestEnv = () => {
+	const rawEnv = config({
+		safe: true,
+		path: "./.env.test",
+	}) as Partial<TestEnv>;
+
+	Object.keys(rawEnv).forEach((key) => {
+		const value = rawEnv[key];
+		if (!value) {
+			const shellEnv = Deno.env.get(key);
+			if (shellEnv) rawEnv[key] = shellEnv;
+		}
+	});
+
+	const nonEmptyStr = makeValidator((x) => {
+		if (x.length === 0) {
+			throw new Error("Expected non-empty string");
+		}
+		return x;
+	});
+
+	return cleanEnv<TestEnv>(
+		rawEnv,
+		{
+			SPOTIFY_CLIENT_ID: nonEmptyStr(),
+			SPOTIFY_CLIENT_SECRET: nonEmptyStr(),
+			SPOTIFY_REFRESH_TOKEN: nonEmptyStr(),
+		},
+	);
+};
