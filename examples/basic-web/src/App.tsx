99aa4244d318edfb90a78b41cb4ed6fec9aa24c1
Artem Melnyk <melkam464@gmail.com>
2023-02-12 18:24:31 +0100

    upd

--- examples/basic-web/src/App.tsx
+++ examples/basic-web/src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import {
 	getCurrentUserProfile,
+	IAuthProvider,
+	ImplicitAuthProvider,
 	ImplicitGrantService,
 	UserPrivate,
 } from "soundify-web-api/web";
@@ -10,46 +12,60 @@ const authService = new ImplicitGrantService({
 	SPOTIFY_REDIRECT_URI: import.meta.env.VITE_SPOTIFY_REDIRECT_URI,
 });
 
-const useSpotifyProfile = () => {
+const authURL = authService.getAuthURL({
+	scopes: ["user-read-email"],
+});
+
+const useSpotifyProfile = (authProvider: IAuthProvider) => {
 	const [userProfile, setUserProfile] = useState<UserPrivate | null>(null);
+	const [isLoading, setLoading] = useState<boolean>(false);
 	const [error, setError] = useState<string | null>(null);
 
 	useEffect(() => {
-		if (userProfile !== null) return;
-
-		try {
-			const { access_token } = authService.getGrantData(location.hash);
-
-			(async () => {
-				try {
-					const user = await getCurrentUserProfile(access_token);
-					setUserProfile(user);
-				} catch (error) {
-					setError(String(error));
-				}
-			})();
-		} catch (error) {
-			const authURL = authService.getAuthURL({
-				scopes: ["user-read-email"],
-			});
-
-			location.replace(authURL);
-		}
+		setLoading(true);
+		(async () => {
+			try {
+				const user = await getCurrentUserProfile(authProvider);
+				setUserProfile(user);
+			} catch (error) {
+				setError(String(error));
+			} finally {
+				setLoading(false);
+			}
+		})();
 	}, []);
 
-	return { userProfile, error };
+	return { userProfile, error, isLoading };
 };
 
 export const App = () => {
-	const { userProfile, error } = useSpotifyProfile();
+	if (location.pathname === "/callback") {
+		const { access_token } = authService.getGrantData(location.hash);
+		localStorage.setItem("SPOTIFY_ACCESS_TOKEN", access_token);
+		location.replace("/");
+		return;
+	}
 
-	return (
-		<>
-			{error
-				? <h1>{error}</h1>
-				: userProfile === null
-				? <div>Redirecting to login...</div>
-				: <h1>Welcome {userProfile.display_name}!</h1>}
-		</>
-	);
+	const accessToken = localStorage.getItem("SPOTIFY_ACCESS_TOKEN");
+	if (!accessToken) {
+		location.replace(authURL);
+		return;
+	}
+
+	const authProvider = new ImplicitAuthProvider({
+		access_token: accessToken,
+		refresh: () => location.replace(authURL),
+	});
+
+	const { userProfile, error, isLoading } = useSpotifyProfile(authProvider);
+
+	if (isLoading) {
+		return <h1>Loading...</h1>;
+	}
+
+	if (error || !userProfile) {
+		return <h1>{error ?? "Unexpected error"}</h1>;
+	}
+
+	return <h1>Welcome {userProfile.display_name}!</h1>;
 };
