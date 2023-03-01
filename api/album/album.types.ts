2e89ede111ac17f2f2f7aaf4c65e6680e7117903
Artem Melnyk <melkam464@gmail.com>
2023-03-01 14:45:39 +0100

    chore: fix comments and add type field for copyrights

--- api/album/album.types.ts
+++ api/album/album.types.ts
@@ -84,6 +84,12 @@ export interface AlbumBase {
 		 * The copyright text for this content.
 		 */
 		text: string;
+		/**
+		 * The type of copyright: \
+		 * C = the copyright \
+		 * P = the sound recording (performance) copyright
+		 */
+		type: string;
 	}[];
 	/**
 	 * A list of the genres the album is associated with.
