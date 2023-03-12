9325d7b4c28427c7bf164b997d39b2541819e708
Artem Melnyk <melkam464@gmail.com>
2023-03-12 19:02:29 +0100

    fix: build and actions

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,7 +1,12 @@
 import { SpecifierMappings } from "https://deno.land/x/dnt@0.33.0/transform.ts";
 import { build, emptyDir } from "https://deno.land/x/dnt@0.33.1/mod.ts";
 
-const version = Deno.args[0]?.replace(/^v/, "");
+const buildType = Deno.args[0] as
+	| "local"
+	| `npm:${"api" | "web" | "node" | "shared"}`
+	| undefined ?? "local";
+
+const version = Deno.args[1]?.replace(/^v/, "");
 
 const buildPackage = async (opts: {
 	packageName: string;
@@ -55,73 +60,98 @@ const buildPackage = async (opts: {
 	});
 };
 
-console.log("\nBuild `@soudnfiy/shared` ...\n");
+const buildShared = async () => {
+	console.log("\nBuild `@soudnfiy/shared` ...\n");
 
-await buildPackage({
-	entryPoint: "./shared/mod.ts",
-	outDir: "./dist/shared/",
-	packageName: "@soundify/shared",
-	environment: "browser",
-	description: "Shared types and functions for soundify packages",
-});
+	await buildPackage({
+		entryPoint: "./shared/mod.ts",
+		outDir: "./dist/shared/",
+		packageName: "@soundify/shared",
+		environment: "browser",
+		description: "Shared types and functions for soundify packages",
+	});
+};
 
-console.log("\nBuild `@soudnfiy/api` ...\n");
+const buildApi = async (type: "local" | "npm") => {
+	console.log("\nBuild `@soudnfiy/api` ...\n");
 
-await buildPackage({
-	entryPoint: "./api/mod.ts",
-	outDir: "./dist/api/",
-	packageName: "@soundify/api",
-	environment: "browser",
-	mappings: {
-		"shared/mod.ts": {
-			name: "@soundify/shared",
-			version: Deno.args[0],
+	await buildPackage({
+		entryPoint: "./api/mod.ts",
+		outDir: "./dist/api/",
+		packageName: "@soundify/api",
+		environment: "browser",
+		mappings: {
+			"shared/mod.ts": {
+				name: "@soundify/shared",
+				version,
+			},
 		},
-	},
-	dependencies: {
-		"@soundify/shared": "link:../shared",
-	},
-	description: "Modern Spotify api wrapper for Node, Deno, and browser 🎧",
-});
-
-console.log("\nBuild `@soudnfiy/web` ...\n");
-
-await buildPackage({
-	entryPoint: "./auth/mod.ts",
-	outDir: "./dist/web/",
-	packageName: "@soundify/web",
-	environment: "browser",
-	mappings: {
-		"auth/platform/platform.deno.ts": "./auth/platform/platform.web.ts",
-		"shared/mod.ts": {
-			name: "@soundify/shared",
-			version: Deno.args[0],
+		dependencies: {
+			"@soundify/shared": type === "local" ? "link:../shared" : version,
 		},
-	},
-	dependencies: {
-		"@soundify/shared": "link:../shared",
-	},
-	description: "Spoitfy authorization for web platform",
-});
+		description: "Modern Spotify api wrapper for Node, Deno, and browser 🎧",
+	});
+};
 
-console.log("\nBuild `@soudnfiy/node` ...\n");
+const buildWeb = async (type: "local" | "npm") => {
+	console.log("\nBuild `@soudnfiy/web` ...\n");
 
-await buildPackage({
-	entryPoint: "./auth/mod.ts",
-	outDir: "./dist/node/",
-	packageName: "@soundify/node",
-	environment: "node",
-	mappings: {
-		"auth/platform/platform.deno.ts": "./auth/platform/platform.node.ts",
-		"node:buffer": "buffer",
-		"node:crypto": "crypto",
-		"shared/mod.ts": {
-			name: "@soundify/shared",
-			version: Deno.args[0],
+	await buildPackage({
+		entryPoint: "./auth/mod.ts",
+		outDir: "./dist/web/",
+		packageName: "@soundify/web",
+		environment: "browser",
+		mappings: {
+			"auth/platform/platform.deno.ts": "./auth/platform/platform.web.ts",
+			"shared/mod.ts": {
+				name: "@soundify/shared",
+				version,
+			},
 		},
-	},
-	dependencies: {
-		"@soundify/shared": "link:../shared",
-	},
-	description: "Spoitfy authorization for nodejs platform",
-});
+		dependencies: {
+			"@soundify/shared": type === "local" ? "link:../shared" : version,
+		},
+		description: "Spoitfy authorization for web platform",
+	});
+};
+
+const buildNode = async (type: "local" | "npm") => {
+	console.log("\nBuild `@soudnfiy/node` ...\n");
+
+	await buildPackage({
+		entryPoint: "./auth/mod.ts",
+		outDir: "./dist/node/",
+		packageName: "@soundify/node",
+		environment: "node",
+		mappings: {
+			"auth/platform/platform.deno.ts": "./auth/platform/platform.node.ts",
+			"node:buffer": "buffer",
+			"node:crypto": "crypto",
+			"shared/mod.ts": {
+				name: "@soundify/shared",
+				version,
+			},
+		},
+		dependencies: {
+			"@soundify/shared": type === "local" ? "link:../shared" : version,
+		},
+		description: "Spoitfy authorization for nodejs platform",
+	});
+};
+
+if (buildType === "local") {
+	await buildShared();
+	await buildApi("local");
+	await buildWeb("local");
+	await buildNode("local");
+	Deno.exit(0);
+}
+
+const buildMap = {
+	"npm:shared": buildShared,
+	"npm:api": buildApi,
+	"npm:web": buildWeb,
+	"npm:node": buildNode,
+};
+
+await buildMap[buildType]("npm");
